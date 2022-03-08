1dd1dee7e15cc8bc693a5766fd3fbc71f084ab33
Allan Cheam Alzula <allan.alzula@gmail.com>
2022-03-08 09:52:00 +0800

    chore: added user details api chore: added enum value for current user chore: update swagger documentation

--- src/_core/decorator/index.ts
+++ src/_core/decorator/index.ts
@@ -1,2 +1,3 @@
 export { IsValidUsernameOrEmail } from './is-valid-username-or-email.decorator';
 export { Cookies } from './cookies.decorator';
+export { CurrentUser } from './current-user.decorator';
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
-  controllers: [],
+  controllers: [UserController],
   exports: [UserService, TypeOrmModule],
 })
 export class UserModule {}
